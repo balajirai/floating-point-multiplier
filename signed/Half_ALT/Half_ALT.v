7a3d6b5e8af79cad2843f22108fe0b6636d68b3c
Balaji <iam.balajirai@gmail.com>
2024-02-26 22:55:05 +0530

    normalization

--- signed/Half_ALT/Half_ALT.v
+++ signed/Half_ALT/Half_ALT.v
@@ -3,19 +3,23 @@ input [15:0]a;
 input [15:0]b;
 
 output sign;
-output [5:0] exp;
+output [4:0] exp;
 output [20:0] frac_norm;
 
 wire [6:0]c;
 wire [6:0]d;
 
-assign c=a[14:10] + 5'b01111;
-assign d=b[14:10] + 5'b01111;
-assign sign=a[15] ^ b[15];
-assign exp=c+d-5'b11110;
+assign c=a[14:10] + 5'b10000;   // biasing
+assign d=b[14:10] + 5'b10000;   // biasing
+
+assign sign=a[15] ^ b[15];      // xor of sign
+
+assign exp=c+d-6'b100000;       // unbiasing
 
 wire [19:0] mant;
 
-assign mant=a[9:0] *b[9:0];
-assign frac_norm = {1'b1, mant};
+assign mant=a[9:0] * b[9:0];    // mantissa multiplication
+
+assign frac_norm = {1'b0, mant};    // normalization
+
 endmodule
